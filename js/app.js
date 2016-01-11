4bf4291a9726c3caf220058f9321c4b63aea8966
Lex Myers <lex.myers@gmail.com>
2016-01-11 13:52:20 -0800

    created ppm object and GNOC method

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 // my js will be in here
 
-// QUACK QUACK QUACK
+
+var pikePlaceMarketLoc = {
+  minCustPerHour: 14,
+  maxCustPerHour: 55,
+  cupsPerCust: 1.2,
+  lbsToGoPerCust: 3.7,
+}
+
+pikePlaceMarketLoc.generateNumOfCust = function() {
+  return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+}
