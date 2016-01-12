eb952b650a15c411094c7e94e17ac3b42aec251f
Lex Myers <lex.myers@gmail.com>
2016-01-12 15:54:45 -0800

    added comments and updated README

--- js/app.js
+++ js/app.js
@@ -29,6 +29,9 @@ function StoreLocation(locName, minCustPerHour, maxCustPerHour, cupsPerCust, lbs
   }
 }
 
+
+// storeArray contains a set of objects, each representing a store location
+
 var storeArray = [];
 
 storeArray.push(new StoreLocation('Pike Place Market', 14, 55, 1.2, 5.7));
@@ -38,53 +41,64 @@ storeArray.push(new StoreLocation('South Lake Union', 35, 88, 1.3, 3.7));
 storeArray.push(new StoreLocation('Sea-Tac Airport', 68, 124, 1.1, 2.7));
 storeArray.push(new StoreLocation('Website Sales', 3, 6, 0, 6.7));
 
+// sectHead is the top of the section that holds the tables
 
 var sectHead = document.getElementById('main');
 
-function createRow(el1Text, el2Text) {
+// renderRow returns HTML for each row of each table, given input of the text
+// for each element. 'header' is a boolean indicating in the row is at the
+// head of the table.
+
+function renderRow(el1Text, el2Text, header) {
   var newRow = document.createElement('tr');
-  var element1 = document.createElement('td');
+  if(header) {
+    var element1 = document.createElement('th');
+    var element2 = document.createElement("th");
+  } else {
+    var element1 = document.createElement('td');
+    var element2 = document.createElement("td");
+  }
   element1.textContent = el1Text;
-  var element2 = document.createElement("td");
   element2.textContent = el2Text;
   newRow.appendChild(element1);
   newRow.appendChild(element2);
   return newRow;
 }
 
-function renderRow(storeLoc, i) {
+// calcRow calculates the stats for each hour, then calls renderRow to
+// generate the HTML
+
+function calcRow(storeLoc, i) {
   storeLoc.genHourlyStatistics();
   var lbsForCups = storeLoc.results.numberOfCustomers / 20;
   var totalLbs = lbsForCups + storeLoc.results.lbsSold;
-  return createRow(timesArray[i], totalLbs.toFixed(1));
+  return renderRow(timesArray[i], totalLbs.toFixed(1), false);
 }
+
+// renderTable creates a table beneath a header with the store name
 
 function renderTable(storeLoc) {
   // create a new header element with the store name
   var newHeader = document.createElement('h2');
   var newHeaderText = document.createTextNode(storeLoc.locName);
   newHeader.appendChild(newHeaderText);
+  // add it to the section
   sectHead.appendChild(newHeader);
   // create a new table
   var newTable = document.createElement('table');
   var newTableHead =document.createElement('thead');
-  // create the top row
-  var topRow = document.createElement('tr');
-  // create header for the row
-  var top1El = document.createElement('th');
-  // put text in there
-  top1El.textContent = 'Time of Day';
-  var top2El = document.createElement('th');
-  top2El.textContent = 'Pounds sold';
-  topRow.appendChild(top1El);
-  topRow.appendChild(top2El);
-  newTableHead.appendChild(topRow);
+  // call renderRow to create a header for the table
+  newTableHead.appendChild(renderRow('Time of Day', 'Pounds Sold', true));
   newTable.appendChild(newTableHead);
+  // generate the data for each row
   for(var i = 0;i < timesArray.length; i++) {
-    newTable.appendChild(renderRow(storeLoc, i));
+    newTable.appendChild(calcRow(storeLoc, i));
   }
+  // ... and finally add the table to the section
   sectHead.appendChild(newTable);
 }
+
+// main loop: generate a table for each object in storeArray
 
 for(var j = 0; j < storeArray.length; j++) {
   renderTable(storeArray[j]);
